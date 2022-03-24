f93ad4c243f3a3933849248a1aaeb3de182d36fd
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-23 22:22:01 -0400

    commit tree 124551f46cd4baabb61d27f3930fad66b46e5521

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name].present?
+      name = params[:name].titlecase
+      students = []
+      students.push(Student.where("first_name = ?", params[:name]))
+      students.push(Student.where("last_name = ?", params[:name]))
+    else
+      students = Student.all
+    end
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
